9635e8f710740c32238b7862b9cbc44a584a1f2d
zjfeng <1849540523@qq.com>
2023-05-31 15:23:48 +0800

    Spring框架

---
+++ spring/src/main/java/com/xxx/Test.java
@@ -0,0 +1,14 @@
+package com.xxx;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        user.say();
+    }
+
+}
---
+++ spring/src/main/java/com/xxx/User.java
@@ -0,0 +1,11 @@
+package com.xxx;
+
+public class User {
+
+    private int age;
+
+    public void say(){
+        System.out.println("say...");
+    }
+
+}
